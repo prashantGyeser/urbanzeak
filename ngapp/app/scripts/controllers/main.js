c14862552b72b5b5e8ad14f6a73371f6e73c5d39
Prashant <prashant@gyeser.com>
2013-11-13 19:14:24 +0530

    Base commit

---
+++ ngapp/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('notesApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
