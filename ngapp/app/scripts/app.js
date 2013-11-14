d3c37324a1fda953e2e566fbf17d92dd58b7ba41
Prashant <prashant@gyeser.com>
2013-11-14 11:54:45 +0530

    Fixed the issue with the width of the row not being taken into account

--- ngapp/app/scripts/app.js
+++ ngapp/app/scripts/app.js
@@ -10,10 +10,6 @@ angular.module('urbanfunhunterApp', [
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/home', {
         templateUrl: 'views/home.html',
         controller: 'HomeCtrl'
       })
