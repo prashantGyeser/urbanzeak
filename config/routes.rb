ee56c77931a2b2164528c7c262508b615a87a084
Prashant <prashant@gyeser.com>
2013-11-16 00:42:12 +0530

    Before trying out the ng-rails-csrf gem. Right now because the two applications are seperate I cannot add <%= csrf_meta_tags %> to the page. So when data is posted to the server the app breaks becuase of the lack of csrf tags. This gem should fix that by setting the csrf tonken in the header of the request

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,13 @@ LetsdineUpdated::Application.routes.draw do
       #devise_for :user, :controllers => {sessions: 'sessions'}
       #devise_for :user, :controllers => { :sessions => "api/v1/sessions" }
       get 'current_user' => 'logged_user#user' #, :as => 'current_user'
-
+      devise_for :users
       #devise_for :users, :skip => :sessions
 
-      devise_scope :user do
-        post "/users/sign_in" => "devise/sessions#create"
-        delete "/users/sign_out" => "devise/sessions#destroy"
-      end
+      #devise_scope :user do
+        #post "/users/sign_in" => "devise/sessions#create"
+        #delete "/users/sign_out" => "devise/sessions#destroy"
+      #end
 
     end
   end
--- ngapp/app/scripts/controllers/login.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('urbanfunhunterApp')
-  .controller('LoginCtrl', function ($scope) {
-
-  });
