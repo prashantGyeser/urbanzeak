e9290f3c92ec024b85341eef4ab84d3bbd9640e7
Prashant <prashant@gyeser.com>
2013-11-28 15:37:34 +0530

    After base rails install along with rspec generator done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Urbanzeak::Application.config.secret_key_base = '1175452358fa1ce6f07d68ec545c5c557930140f0511ebca4f1870218ea75b1503dc7de99b0b4188df2f145c08146e657d8d0ff13b8af21fde281ab2089cd923'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Urbanzeak::Application.config.session_store :cookie_store, key: '_urbanzeak_session'
