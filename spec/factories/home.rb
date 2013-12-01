bcc9b83274e70b201cc1ee74d664d5776acb12ea
Prashant <prashant@gyeser.com>
2013-12-01 16:12:22 +0530

    Before adding the manage eexperiences code

--- spec/factories/home.rb
+++ spec/factories/home.rb
@@ -1,12 +1,9 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
-  factory :user do
+  factory :normal_user do
     email 'john@doe.com'
     first_name 'John'
     last_name 'Doe'
     city 'sometestEmail@test.com'
     password 'password@123'
   end
-
 end
