527715d017f7dd4fa43ecabb06b52608556efea4
Prashant <prashant@gyeser.com>
2013-12-02 20:32:13 +0530

    Added a filter for the images on the homepage

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,27 +1,19 @@
-jQuery(function ()
-{
-    jQuery("#cityFilter").autocomplete({
-        source: function (request, response) {
-            jQuery.getJSON(
-                "http://gd.geobytes.com/AutoCompleteCity?callback=?&q="+request.term,
-                function (data) {
-                    response(data);
-                }
-            );
-        },
-        minLength: 3,
-        select: function (event, ui) {
-            var selectedObj = ui.item;
-            jQuery("#cityFilter").val(selectedObj.value);
-            return false;
-        },
-        open: function () {
-            jQuery(this).removeClass("ui-corner-all").addClass("ui-corner-top");
-        },
-        close: function () {
-            jQuery(this).removeClass("ui-corner-top").addClass("ui-corner-all");
-        }
+$(document).ready(function(){
+
+    $('.expListingImg').each(function(index){
+        Caman(this, function(){
+            this.sunrise();
+            this.render();
+        });
+
     });
-    jQuery("#cityFilter").autocomplete("option", "delay", 100);
+    /*
+    $('.expListingImg').caman(function(){
+        this.sunrise();
+        this.render();
+    });
+    */
+
 });
+
 
