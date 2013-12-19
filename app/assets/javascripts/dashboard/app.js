913b45452d2f76b92fcfe5408ceae335f6e019a2
Prashant <prashant@gyeser.com>
2013-12-19 22:00:07 +0530

    Got the list of people online to show when a user lands on the hosts page. Now need to figure out what to do once they leave the page i.e. the client leaves the experience page.

--- app/assets/javascripts/dashboard/app.js
+++ app/assets/javascripts/dashboard/app.js
@@ -3,26 +3,12 @@ var chatApp = angular.module('chatApp', ["firebase"]);
 chatApp.controller('ChatCtrl', ['$scope', '$firebase', function($scope, $firebase){
     var hostUrl = "https://urbanzeak.firebaseio.com/hosts/1/customers/";
     var hostRef = new Firebase("https://urbanzeak.firebaseio.com/hosts/1/customers/");
-    var leadsListUrl = 'https://urbanzeak-users-development.firebaseio.com/1/';
-    var leadUrl = 'https://urbanzeak-users-development.firebaseio.com/1/lead'
-    var presenceUrl = 'https://urbanzeak-users-development.firebaseio.com/.info/connected'
+    var leadsUrl = 'https://urbanzeak-users-development.firebaseio.com/1/leads'
+    //var presenceUrl = 'https://urbanzeak-users-development.firebaseio.com/.info/connected'
 
     // Firebase initializations
-    var leadRef = new Firebase(leadUrl);
-    var presenceRef = new Firebase(presenceUrl);
+    var leadsRef = new Firebase(leadsUrl);
 
-    presenceRef.on('value', function(snap){
-        if (snap.val()){
-            console.log('Ok there is a connection', snap.val());
-            $scope.test = snap.val();
-
-            leadRef.push("hello");
-
-        }
-    })
-
-
-
-
+    $scope.leads = $firebase(leadsRef);
 
 }]);
