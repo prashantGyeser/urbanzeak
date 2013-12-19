e027979af11d584cc7f78fc6b08957526ea9b99d
Prashant <prashant@gyeser.com>
2013-12-20 02:08:42 +0530

    Added a spinner to show data is loading when getting the chat messages. Bug: Going back and clicking on a visitor id does nothing i.e. it does not get the data from the server. Need to figure out why it is happening

--- app/assets/javascripts/dashboard/app.js
+++ app/assets/javascripts/dashboard/app.js
@@ -1,6 +1,6 @@
 var chatApp = angular.module('chatApp', ["firebase"]);
 
-chatApp.controller('ChatCtrl', ['$scope', '$firebase', function($scope, $firebase){
+chatApp.controller('ChatCtrl', ['$scope', '$firebase', '$q', function($scope, $firebase, $q){
     var hostUrl = "https://urbanzeak.firebaseio.com/hosts/1/customers/";
     var hostRef = new Firebase("https://urbanzeak.firebaseio.com/hosts/1/customers/");
     var leadsUrl = 'https://urbanzeak-users-development.firebaseio.com/1/leads'
@@ -13,10 +13,17 @@ chatApp.controller('ChatCtrl', ['$scope', '$firebase', function($scope, $firebas
 
     $scope.getMessages = function(leadId){
         console.log("the id clicked was:", leadId);
+        // Setting a loading value so that it can be used in the view to show a spinner
+        $scope.loading = true;
+
         var messagesUrl = 'https://urbanzeak-chat-development.firebaseio.com/hosts/1/leads/' + leadId + '/messages/';
         var messagesRef = new Firebase(messagesUrl);
 
         $scope.messages = $firebase(messagesRef);
+
+        $scope.messages.$on('loaded', function(){
+            $scope.loading = false;
+        });
 
     }
 
