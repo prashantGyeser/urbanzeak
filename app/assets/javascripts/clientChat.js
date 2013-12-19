46c4ab82af1e01cab2fd0aed84b33fe6ad119c6c
Prashant <prashant@gyeser.com>
2013-12-19 19:39:15 +0530

    Added code to set the presence of the lead that views a page

--- app/assets/javascripts/clientChat.js
+++ app/assets/javascripts/clientChat.js
@@ -8,8 +8,6 @@ function makeCustomClientid()
     {
         text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
-
-
     return text;
 }
 
@@ -18,35 +16,60 @@ var chatApp = angular.module('clientchatApp', ["firebase"]);
 
 chatApp.controller('ClientchatCtrl', ['$scope', '$firebase', function($scope, $firebase){
     // Creating a uid for this user
-    var userId = makeCustomClientid();
+    var leadId = makeCustomClientid();
+
+    // Todo: Change this to the actual host id for this experience
+    var currentExperienceHostId = 1;
 
     var baseCurrentHostUrl = 'https://urbanzeak.firebaseio.com/hosts/1/';
-    var presenceUrl = baseCurrentHostUrl + 'usersViewing/';
     var currentUserUrl = baseCurrentHostUrl + 'customers/';
     var currentUserMessagesUrl = currentUserUrl + 'messages/';
 
+    var leadsUrl = 'https://urbanzeak-users-development.firebaseio.com/';
+    var connectedUrl = leadsUrl + '.info/connected';
 
-    //var hostRef = new Firebase("https://urbanzeak.firebaseio.com/hosts/1/customers/");
+    var hostLeadListUrl = leadsUrl + currentExperienceHostId + '/leads/';
+
+    // Setting a ref to the leads table
+    var onlineRef = new Firebase(hostLeadListUrl);
+    var connectedRef = new Firebase(connectedUrl);
+
+    // Creating a reference to the leads table and adding a lead as soon as they land on the page
+    connectedRef.on('value', function(snap){
+        console.log('Conn:', snap.val());
+
+        if(snap.val() === true){
+            // The client is connected. Setup presence state and tell the server to set a timestamp when they leave
+            var leadRef = onlineRef.child(leadId);
+            var connRef = leadRef.child('online').push(1);
+
+            // Actions to take on user disconnect
+            connRef.onDisconnect().remove();
+
+            // Store the last seen time stamp for this lead
+            leadRef.child('logout').onDisconnect().set(Firebase.ServerValue.TIMESTAMP);
+
+        }
+
+    });
 
     // Getting all the people who have ever visited the site here.
     // Todo: Show only people that are currently on the site.
     //$scope.customers = $firebase(hostRef);
     //console.log("the values returned are:", $scope.customers);
 
-    // Adding a user to the list of users when they view the page.
-    var presenceRef = new Firebase(presenceUrl);
     //$scope.usersOnPage = $firebase(presenceRef);
     //$scope.usersOnPage.$add({userId: userId});
 
     // Getting messages for this user
-    var messagesRef = new Firebase(currentUserMessagesUrl);
+    //var messagesRef = new Firebase(currentUserMessagesUrl);
 
-    $scope.messages = $firebase(messagesRef);
+    //$scope.messages = $firebase(messagesRef);
 
     // Creating a reference to the messages for this user.
-    $scope.addMessage = function(e) {
-        if(e.keyCode != 13) return;
-        $scope.messages.$add({message: $scope.message});
-        $scope.message = '';
-    }
+    //$scope.addMessage = function(e) {
+        //if(e.keyCode != 13) return;
+        //$scope.messages.$add({message: $scope.message});
+        //$scope.message = '';
+    //}
 }]);
--- app/assets/javascripts/dashboard/app.js
+++ app/assets/javascripts/dashboard/app.js
@@ -3,9 +3,26 @@ var chatApp = angular.module('chatApp', ["firebase"]);
 chatApp.controller('ChatCtrl', ['$scope', '$firebase', function($scope, $firebase){
     var hostUrl = "https://urbanzeak.firebaseio.com/hosts/1/customers/";
     var hostRef = new Firebase("https://urbanzeak.firebaseio.com/hosts/1/customers/");
+    var leadsListUrl = 'https://urbanzeak-users-development.firebaseio.com/1/';
+    var leadUrl = 'https://urbanzeak-users-development.firebaseio.com/1/lead'
+    var presenceUrl = 'https://urbanzeak-users-development.firebaseio.com/.info/connected'
 
-    // Getting all the people who have ever visited the site here.
-    // Todo: Show only people that are currently on the site.
-    //$scope.customers = $firebase(hostRef);
+    // Firebase initializations
+    var leadRef = new Firebase(leadUrl);
+    var presenceRef = new Firebase(presenceUrl);
+
+    presenceRef.on('value', function(snap){
+        if (snap.val()){
+            console.log('Ok there is a connection', snap.val());
+            $scope.test = snap.val();
+
+            leadRef.push("hello");
+
+        }
+    })
+
+
+
+
 
 }]);
