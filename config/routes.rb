57a3335e7f6a335b1745a101b3288070ea0d9394
Prashant <prashant@gyeser.com>
2013-12-26 00:34:31 +0530

    Added a route so that the email_processor post url can be added to Mandrill. This error was becuase Mandrill sends a HEAD request but griddler only has a POST url by default. So added a get to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Urbanzeak::Application.routes.draw do
 
   resources :messages
 
+  get "/email_processor", to: proc { [200, {}, ["OK"]] }, as: "mandrill_head_test_request"
+
   get "chat/index"
   namespace :dashboard do
     root "home#index"
