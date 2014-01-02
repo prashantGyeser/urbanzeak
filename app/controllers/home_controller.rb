a856be1e446f151f2161dca659b353dad65f9cd7
Prashant <prashant@gyeser.com>
2014-01-02 12:26:59 +0530

    Removed the country part from the home index controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,31 +1,6 @@
 class HomeController < ApplicationController
   def index
-
-    if request.ip == '127.0.0.1'
-      #@experiences = Experience.first(10)
-      @experiences = Experience.all
-    else
-      location = Geocoder.search(request.ip)
-      @experiences = Experience.where("city like ?", "%#{location[0].country}%")
-
-      @country = request.location.country
-
-      if @experiences.empty?
-        @experiences = Experience.first(9)
-      end
-
-    end
-
-    if params[:city].nil?
-      @experiences = Experience.all
-    else
-      @experiences = Experience.where("city like ?", "%#{params[:city]}%")
-    end
-
-    #@experiences = @experiences.in_groups_of(3)
-
-    logger.debug "The content in the expereinces array is: #{@experiences.inspect}"
-
+    @experiences = Experience.all
 
     respond_to do |format|
       format.html { render action: 'index' }
