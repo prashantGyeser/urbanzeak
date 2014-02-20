7b25faa14cba229d1bba52f3a994ca727b49220d
Prashant <prashant@gyeser.com>
2014-02-20 11:39:42 +0530

    Fixed the issue where the experience image was not being assocaited with the actual image that was uploaded

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -131,6 +131,6 @@ class ExperiencesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def experience_params
       #params.require(:experience).permit(:name, :description, :price, :exp_date, :exp_time, :latitude, :longitude, :city, exp_images_attributes: [:url])
-      params.require(:experience).permit(:name, :description, :price, :things_to_remember, :line_one, :line_two, :state, :city, :land_mark, :country, :exp_date, :exp_time, :template_id)
+      params.require(:experience).permit(:name, :description, :price, :things_to_remember, :line_one, :line_two, :state, :city, :land_mark, :country, :exp_date, :exp_time, :template_id, :random_id)
     end
 end
