4bc1d8c5adf5129fc3016cf985362a44e85f1b90
Prashant <prashant@gyeser.com>
2014-02-23 15:29:16 +0530

    Storing the acutal email address that comes in

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -3,7 +3,7 @@ class EmailProcessor
     #Message.create!({ to: email.to, from: email.from, subject: email.subject, body: email.body, raw_text: email.raw_text, raw_html: email.raw_html, raw_body: email.raw_body, headers: email.headers, raw_headers: email.raw_headers })
 
     @message = Message.new
-    @message.to = email.to[:email]
+    @message.to = email.to.email
     @message.from = email.from
     @message.subject = email.subject
     @message.body = email.body
