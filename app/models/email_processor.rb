2c3a2d0ece7b5bfdf739903e27d3d0422240b9f1
Prashant <prashant@gyeser.com>
2014-02-23 12:28:52 +0530

    Stripped down fields to only the required ones in the email processor

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,5 +1,12 @@
 class EmailProcessor
   def self.process(email)
-    Message.create!({ to: email.to, from: email.from, subject: email.subject, body: email.body, raw_text: email.raw_text, raw_html: email.raw_html, raw_body: email.raw_body, headers: email.headers, raw_headers: email.raw_headers })
+    #Message.create!({ to: email.to, from: email.from, subject: email.subject, body: email.body, raw_text: email.raw_text, raw_html: email.raw_html, raw_body: email.raw_body, headers: email.headers, raw_headers: email.raw_headers })
+
+    @message = Message.new
+    @message.to = email.to
+    @message.from = email.from
+    @message.subject = email.subject
+    @message.body = email.body
+    @message.save
   end
 end
