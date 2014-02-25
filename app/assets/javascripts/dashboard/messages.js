6d6990a04b9e4afec35398e2a5900da4df1ad4a1
Prashant <prashant@gyeser.com>
2014-02-26 00:42:47 +0530

    Updated the layout for the messages section. bEfore adding the code to submit the content and create a new mesage

--- app/assets/javascripts/dashboard/messages.js
+++ app/assets/javascripts/dashboard/messages.js
@@ -1,9 +1,43 @@
 var ready;
 ready = function() {
 
-    $('tr').click(function(){
-        var messageIdSelected = $(this).data('message-id');
-        window.location = "/dashboard/messages/" + messageIdSelected;
+    /** Text Editor **/
+//$('#message').wysihtml5();
+
+    /** Events **/
+    $('#btn-new-ticket').click( function() {
+        $('#new-ticket-wrapper').slideToggle("fast","linear")
+    })
+
+    $('#btn-close-ticket').click( function() {
+        $('#new-ticket-wrapper').slideToggle("fast","linear")
+    });
+
+    $('.grid .actions a.remove').on('click', function () {
+        var removable = jQuery(this).parents(".grid");
+        if (removable.next().hasClass('grid') || removable.prev().hasClass('grid')) {
+            jQuery(this).parents(".grid").remove();
+        } else {
+            jQuery(this).parents(".grid").parent().remove();
+        }
+    });
+
+    $('.grid .clickable').on('click', function () {
+        var el = jQuery(this).parents(".grid").children(".grid-body");
+        el.slideToggle(200);
+    });
+
+    $('.form-control').bind("enterKey",function(e){
+        alert('k ebnter pressed');
+    });
+
+    $('.form-control').keypress(function(event){
+
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode == '13'){
+            alert('You pressed a "enter" key in textbox');
+        }
+
     });
 
 };
