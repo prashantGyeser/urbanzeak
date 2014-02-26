bce8e2eccba72dcc0417630ee2cccef1da95b183
Prashant <prashant@gyeser.com>
2014-02-26 20:29:37 +0530

    Fixed the issue where the right email was not getting stored when an email came in

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -3,9 +3,11 @@ class EmailProcessor
     #Message.create!({ to: email.to, from: email.from, subject: email.subject, body: email.body, raw_text: email.raw_text, raw_html: email.raw_html, raw_body: email.raw_body, headers: email.headers, raw_headers: email.raw_headers })
 
     to = email.to[0][:email]
+    conversation = Conversation.find(to.partition('@').first.to_i)
+    host = User.find(conversation.user_id)
 
     @message = Message.new
-    @message.to = to
+    @message.to = host.email
     @message.from = email.from
     @message.subject = email.subject
     @message.body = email.body
