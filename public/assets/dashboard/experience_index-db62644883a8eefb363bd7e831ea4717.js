ca7fe6077fe5b101b317eeee34e89dda7063959b
Prashant <prashant@gyeser.com>
2014-03-18 21:11:04 +0530

    Changed the logo image tag

---
+++ public/assets/dashboard/experience_index-db62644883a8eefb363bd7e831ea4717.js
@@ -0,0 +1,1 @@
+var ready;ready=function(){$(".review_comment").hide(),$(".view_review").click(function(e){e.preventDefault(),$(".review_comment").hide();var i=$(this).attr("href");console.log("The value in the var is:",i),$(i).show()}),$(".review_select").click(function(){alert("K clicked")})},$(document).ready(ready),$(document).on("page:load",ready);
