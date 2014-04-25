88c8added8c27928f79fab281844b69d2e4451ba
Prashant <prashant@gyeser.com>
2014-04-25 17:05:37 +0530

    Prevented the marker on the experience show page from being draggable

--- app/assets/javascripts/expShow/google_maps.js
+++ app/assets/javascripts/expShow/google_maps.js
@@ -29,70 +29,18 @@ $(document).ready(function() {
         streetViewControl : false,
         mapTypeControl: false,
         overviewMapControl: false,
+          draggable: false
 
       });
 	  // Add a random mark
 	  setTimeout( function(){
 		  map.addMarker({
-				  lat: latitude,
-				  lng: longitude,
-				  animation: google.maps.Animation.DROP,
-				  draggable:true,
-				  title: 'New marker'
+            lat: latitude,
+            lng: longitude,
+            animation: google.maps.Animation.DROP,
+            draggable: false
 		  });
 	  },3000);
-	  //Initialize Context Menu
-	    map.setContextMenu({
-        control: 'map',
-        options: [{
-          title: 'Add marker',
-          name: 'add_marker',
-          action: function(e){
-            console.log(e.latLng.lat());
-            console.log(e.latLng.lng());
-            this.addMarker({
-              lat: e.latLng.lat(),
-              lng: e.latLng.lng(),
-			  animation: google.maps.Animation.DROP,
-			  draggable:true,
-              title: 'New marker'
-            });
-            this.hideContextMenu();
-          }
-        }, {
-          title: 'Center here',
-          name: 'center_here',
-          action: function(e){
-            this.setCenter(e.latLng.lat(), e.latLng.lng());
-          }
-        }]
-      });
-      map.setContextMenu({
-        control: 'marker',
-        options: [{
-          title: 'Center here',
-          name: 'center_here',
-          action: function(e){
-            this.setCenter(e.latLng.lat(), e.latLng.lng());
-          }
-        }]
-      });
-	        map.travelRoute({
-        origin: [-12.044012922866312, -77.02470665341184],
-        destination: [-12.090814532191756, -77.02271108990476],
-        travelMode: 'driving',
-        step: function(e){
-          $('#instructions').append('<li>'+e.instructions+'</li>');
-          $('#instructions li:eq('+e.step_number+')').delay(450*e.step_number).fadeIn(200, function(){
-            map.drawPolyline({
-              path: e.path,
-              strokeColor: '#131540',
-              strokeOpacity: 0.6,
-              strokeWeight: 6
-            });
-          });
-        }
-      });
 	  $("#map-zoom-out").click(function() {
 		 map.zoomOut(1);
 	  });
