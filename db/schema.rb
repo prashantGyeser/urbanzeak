67e2ceb31ccd42091c509954bd746add14f0c3f4
Prashant <prashant@gyeser.com>
2014-04-26 20:05:09 +0530

    Added a name field to the about seciont form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140423214056) do
+ActiveRecord::Schema.define(version: 20140426143156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -142,6 +142,7 @@ ActiveRecord::Schema.define(version: 20140423214056) do
     t.datetime "updated_at"
     t.string   "avatar"
     t.integer  "user_id"
+    t.string   "name"
   end
 
   add_index "hosts", ["user_id"], name: "index_hosts_on_user_id", using: :btree
