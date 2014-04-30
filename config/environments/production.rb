f452c81316c1ba7790a12d6716a906982a2d1231
Prashant <prashant@gyeser.com>
2014-05-01 00:01:28 +0530

    Added a reset password link in the sign in page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,7 @@ Urbanzeak::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   #config.action_controller.asset_host = "d2962b05f65fka.cloudfront.net"
 
+  # Setting default host for devise
+  config.action_mailer.default_url_options = {:host => "urbanzeak.com"}
+
 end
