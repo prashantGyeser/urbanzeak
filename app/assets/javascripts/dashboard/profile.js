5507bf812406ad6fd4c16616ebf0790d05821e66
Prashant <prashant@gyeser.com>
2014-05-06 20:10:22 +0530

    Fixed the issue where I had hardcoded the host id for delete picture secion in the profile page

--- app/assets/javascripts/dashboard/profile.js
+++ app/assets/javascripts/dashboard/profile.js
@@ -5,9 +5,9 @@ ready = function() {
 
     $('#delete-profile-picture').click(function(e){
         e.preventDefault();
-        var host_id = $('');
+        var host_id = $('#delete-profile-picture').attr('data-host-id');
 
-        $.post( "delete-picture", { host_id: 6 }, function(data){
+        $.post( "delete-picture", { host_id: host_id }, function(data){
             $('#profile-picture').hide();
             $('#delete-profile-picture').hide();
             $('.avatar-uploader').show();
