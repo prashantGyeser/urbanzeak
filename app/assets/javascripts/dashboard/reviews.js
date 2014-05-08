8b3f00905718b87bc23e6238bad2107298cdfb18
Prashant <prashant@gyeser.com>
2014-05-08 17:58:58 +0530

    Added a message to the dashboard page telling the host the review has been added to their experience show page. The message still needs work

--- app/assets/javascripts/dashboard/reviews.js
+++ app/assets/javascripts/dashboard/reviews.js
@@ -5,8 +5,11 @@ $(document).ready(function(){
             // Todo: Send the data to the server and check this box
             // Todo: Check if a host has already selected 3 reviews. If yes throw an error
             var review_selected = $(this).val();
+            var selected_checkbox = $(this);
             $.post('/dashboard/reviews/add_review_to_show', {review_id: review_selected}, function(data){
-
+                    var content_to_prepend = '<div class="alert alert-success">                <button data-dismiss="alert" class="close"></button> Success: The review has been added to your page</div>';
+                    $('.alert-success').remove();
+                    selected_checkbox.parents('.grid-body').prepend(content_to_prepend);
                 },
                 'json'
             );
--- spec/features/dashboard/reviews/index_spec.rb
+++ spec/features/dashboard/reviews/index_spec.rb
@@ -4,8 +4,8 @@ feature 'Reviews' do
 
   background do
     user = FactoryGirl.create(:user)
-    experience = FactoryGirl.create(:experience, :user_id => user.id)
-    @review = FactoryGirl.create(:review, :experience_id => experience.id)
+    @experience = FactoryGirl.create(:experience, :user_id => user.id)
+    @review = FactoryGirl.create(:review, :experience_id => @experience.id)
     visit '/users/sign_in'
     within('#new_user') do
       fill_in 'user_email', with: user.email
@@ -22,6 +22,14 @@ feature 'Reviews' do
   scenario 'See all reviews' do
     find('#reviews_nav').click
     expect(page).to have_content @review.comment
+  end
+
+  scenario 'Selected review should show on the experience show page', :js => true do
+    pending 'Clicking on the review in the experience should show it on the experience show page'
+  end
+
+  scenario 'Selecting a review should show a message telling me the review is shown on the experience show page' do
+    pending
   end
 
 end
