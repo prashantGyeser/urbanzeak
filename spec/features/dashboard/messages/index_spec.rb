1cb8078e8f675ce24607d6cdafba8a8b8e5e175a
Prashant <prashant@gyeser.com>
2014-05-08 17:05:51 +0530

    Removed the landmark line from the experience show page

--- spec/features/dashboard/messages/index_spec.rb
+++ spec/features/dashboard/messages/index_spec.rb
@@ -20,47 +20,55 @@ feature 'I should be able to see and reply to messages' do
     expect(page).to have_content "Message from #{@conversation.customer_name}"
   end
 
-  scenario 'I should see a list of conversations on the messages page in the following order: latest to oldest', :js => true do
-    pending
-  end
-
   scenario 'I should be able to click on a conversation and see all the messages', :js => true do
     visit '/dashboard/messages'
     find('.clickable', :text => "Message from #{@conversation.customer_name}").click
     expect(page).to have_content(@conversation.sender_email_id)
   end
 
-  scenario 'I should be able to close all the messages in case it is open', :js => true do
-    pending
-  end
-
-  scenario 'I should be able to reply to a message', :js => true do
-    pending
-  end
+  scenario 'I should be able to go to a different page after coming to the messages page' do
+    visit '/dashboard/messages'
+    find('#experiences_nav').click
+    expect(page).to have_content "Experiences"
+    end
 
   scenario 'I should see the messages when I go to another page and come back', :js => true do
     visit '/dashboard/messages'
     find('#experiences_nav').click
-    #find(:xpath, "//a[@href='/dashboard/experiences']").click
+    find('#messages_nav').click
+    find('.clickable', :text => "Message from #{@conversation.customer_name}").click
+    expect(page).to have_content(@conversation.sender_email_id)
   end
 
-  scenario 'When I click the icon, the conversation list to expand', :js => true do
+  scenario 'I should see a list of conversations on the messages page in the following order: latest to oldest', :js => true do
     pending
   end
 
-  scenario 'The input box should be a text area', :js => true do
+  scenario 'I should be able to close all the messages in case it is open' do
     pending
   end
 
-  scenario 'Clicking the reply button should send out the messages', :js => true do
+  scenario 'I should be able to reply to a message' do
     pending
   end
 
-  scenario 'When I reply the end customer should get an email with my reply', :js => true do
+  scenario 'When I click the icon, the conversation list to expand' do
     pending
   end
 
-  scenario 'When an end customer sends me a message I should see it in my list of conversations', :js => true do
+  scenario 'The input box should be a text area' do
+    pending
+  end
+
+  scenario 'Clicking the reply button should send out the messages' do
+    pending
+  end
+
+  scenario 'When I reply the end customer should get an email with my reply' do
+    pending
+  end
+
+  scenario 'When an end customer sends me a message I should see it in my list of conversations' do
     pending
   end
 
