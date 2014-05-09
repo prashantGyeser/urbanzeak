60056a19f6e036c545e8770f9af5ed2cf2df23f9
Prashant <prashant@gyeser.com>
2014-05-09 16:09:05 +0530

    Changed the reply input to a text area instead of a input box

--- app/assets/javascripts/dashboard/messages.js
+++ app/assets/javascripts/dashboard/messages.js
@@ -58,26 +58,6 @@ ready = function() {
         });
     });
 
-    $('.form-control').keypress(function(event){
-
-    var keycode = (event.keyCode ? event.keyCode : event.which);
-        if(keycode == '13'){
-            var conversation_reply_to = $(this).siblings('.hidden_conversation_id').val();
-            var message = $(this).val();
-
-            $.post( "messages/create", { message: message, conversation_id: conversation_reply_to }, function(data){
-                contentToAppend = '<div class="post p-b-20">' + '<h4><span class="semi-bold">You</span> said:</h4>' + '<div class="info-wrapper">' + '<div class="info">' + message + '</div>' + '<div class="clearfix"></div>' + '</div>' + '<div class="clearfix"></div>' + '</div>';
-                var divToAppendBefore = $(this).parent().parent().parent().parent().parent();
-
-
-                $(contentToAppend).insertBefore( divToAppendBefore );
-            });
-
-
-        }
-
-    });
-
 };
 
 $(document).ready(ready);
