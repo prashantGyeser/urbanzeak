12e622e748e0cca0f3a4d26953e72bc075aad201
Prashant <prashant@gyeser.com>
2014-05-10 21:19:03 +0530

    Added notifications for messages sent

--- app/assets/javascripts/dashboard/messages.js
+++ app/assets/javascripts/dashboard/messages.js
@@ -37,20 +37,54 @@ ready = function() {
         var conversation_reply_to = $(this).siblings('.hidden_conversation_id').val();
         var message = $(this).siblings('.form-control').val();
         var element_selected = $(this);
-//        contentToAppend = '<div class="post p-b-20">' + '<h4><span class="semi-bold">You</span> said:</h4>' + '<div class="info-wrapper">' + '<div class="info">' + message + '</div>' + '<div class="clearfix"></div>' + '</div>' + '<div class="clearfix"></div>' + '</div>';
-//        var divToAppendBefore = $(this).parent().parent().parent().parent().parent();
-//
-//
-//        $(contentToAppend).insertBefore( divToAppendBefore );
+        // Disabling the button after post
+        if(message){
 
-
-        $.post( "messages/create", { message: message, conversation_id: conversation_reply_to }, function(data){
+        }
+        else
+        {
+            var divToAppendBefore = element_selected.parent().parent().parent().parent().parent();
+            var failure_content = '<div class="alert alert-error"><button data-dismiss="alert" class="close"></button>Danger: You cannot send an empty message!</div>'
+            var parent_container = element_selected.parent();
+            $('.alert').remove();
+            $(failure_content).prependTo( divToAppendBefore );
+            return false;
+        }
+        $.post( "messages/create", { message: message, conversation_id: conversation_reply_to })
+            .done(function(e){
             contentToAppend = '<div class="post p-b-20">' + '<h4><span class="semi-bold">You</span> said:</h4>' + '<div class="info-wrapper">' + '<div class="info">' + message + '</div>' + '<div class="clearfix"></div>' + '</div>' + '<div class="clearfix"></div>' + '</div>';
             var divToAppendBefore = element_selected.parent().parent().parent().parent().parent();
 
+            var success_content = '<div class="alert alert-success"><button data-dismiss="alert" class="close"></button>Success: Your message was sent. Do not trust us, look above!</div>'
             $(contentToAppend).insertBefore( divToAppendBefore );
-
+            element_selected.parent().find('textarea').val('');
+            var parent_container = element_selected.parent();
+            $('.alert').remove();
+            $(success_content).prependTo( divToAppendBefore );
+        })
+            .fail( function(xhr, textStatus, errorThrown) {
+                var failure_content = '<div class="alert alert-error"><button data-dismiss="alert" class="close"></button>Danger: Oops! Something went wrong, please try again later.</div>'
+                var parent_container = element_selected.parent();
+                $('.alert').remove();
+                $(failure_content).prependTo( divToAppendBefore );
         });
+
+        $('#send_conversation').click(function(e){
+            e.preventDefault();
+
+            $.post('/conversations/create', $('form#new_conversation').serialize())
+                .done( function(msg) {
+                    $('.new_conversation_body').hide();
+                    $('#conversation_created_success').show();
+                    $('#send_conversation').attr('disabled', true);
+                    $('#send_conversation').addClass('disabled');
+                } )
+                .fail( function(xhr, textStatus, errorThrown) {
+                    $('#conversation_created_fail').show();
+                });
+        });
+
+
     });
 
 };
