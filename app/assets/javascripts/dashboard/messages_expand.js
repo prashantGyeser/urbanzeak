017189fa70b681352a2361e765645c68b54c5c86
Prashant <prashant@gyeser.com>
2014-05-10 18:28:50 +0530

    Fixed the issue where clicking on the conversation in the dashboard/messages page was not opening the messages part properly. Still need to check and fix the issue where the data is sent to the server as a response

--- app/assets/javascripts/dashboard/messages_expand.js
+++ app/assets/javascripts/dashboard/messages_expand.js
@@ -1,17 +1,9 @@
 var ready;
 ready = function() {
-    $('.grid .clickable').off('click').on('click', function (event) {
-    //$('.grid .clickable').unbind('click').click(function(){
+    //$('.grid .clickable').off('click').on('click', function (event) {
+    $('.grid .clickable').unbind('click').click(function(){
         var el = jQuery(this).parents(".grid").children(".grid-body");
-        //el.slideToggle(200);
-        event.stopPropagation(); //<-- has no effect to the described behavior
-        console.log(event.currentTarget);
-//        if (el.is(":visible")){
-//            el.hide();
-//        }
-//        else {
-//            el.show();
-//        }
+        el.slideToggle(200);
     });
 };
 
