c7490a9e99de8e6fae7b8f579b92a4f6864d4523
Prashant <prashant@gyeser.com>
2014-05-14 00:29:46 +0530

    Precompiled assets

--- app/assets/javascripts/dashboard/application.js
+++ app/assets/javascripts/dashboard/application.js
@@ -26,7 +26,7 @@
 //= require plugins/bootstrap-datepicker/js/bootstrap-datepicker
 //= require plugins/bootstrap-timepicker/js/bootstrap-timepicker.min
 //= require plugins/bootstrap-select2/select2.min
-//= require chardinjs
+//= require plugins/chardinjs/chardinjs.min
 //= require plugins/sisyphus/sisyphus
 //= require dashboard/core
 //= require dashboard/chat
