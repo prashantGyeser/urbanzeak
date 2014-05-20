9bc59087230dc16af8b2b9be8cbd03214d761328
Prashant <prashant@gyeser.com>
2014-05-20 22:23:04 +0530

    Added a preview for the founder welcome

--- spec/email_previews/user_emailer_preview.rb
+++ spec/email_previews/user_emailer_preview.rb
@@ -1,8 +1,12 @@
 class UserMailerPreview < ActionMailer::Preview
   def signup_confirmation
     user = FactoryGirl.build(:user)
-
     UserMailer.signup_confirmation(user)
-
   end
+
+  def founder_welcome
+    user = User.first
+    UserMailer.founder_welcome(user, Time.now)
+  end
+
 end
